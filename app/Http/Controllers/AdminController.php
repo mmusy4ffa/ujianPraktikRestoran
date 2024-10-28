dfd74064c636f28fad760a17afd93b1e0bf7d40a
fiftysnd <llgaman008@gmail.com>
2024-10-28 20:55:23 +0700

    refresh admin front end

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -20,7 +20,7 @@ class AdminController extends Controller
 
     public function AdminDashboard()
     {
-        return view('admin.admin_dashboard');
+        return view('admin.index');
     }
 
     public function AdminLoginSubmit(Request $request)
